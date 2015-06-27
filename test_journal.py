0efa8f7ec81762292ad955965d8eef0e07ad3422
Tyler Peek <tyler.peek@eagles.ewu.edu>
2015-06-26 20:16:24 -0700

    Comments Added comments to tests

--- test_journal.py
+++ test_journal.py
@@ -78,6 +78,13 @@ def auth_req(request):
     return req
 
 
+def login_helper(username, password, app):
+    #  Encapsulate app login for reuse in tests
+    #  Accept all status codes so that we can make assertions in tests
+    login_data = {'username': username, 'password': password}
+    return app.post('/login', params=login_data, status='*')
+
+
 def test_write_entry(db_session):
     kwargs = {'title': "Test Title", 'text': "Test entry text"}
     kwargs['session'] = db_session
@@ -106,7 +113,13 @@ def test_write_entry(db_session):
         assert getattr(entry, auto, None) is not None
 
 
+#  -----------------
+#  Test the Database
+#  -----------------
+
 def test_entry_no_title_fails(db_session):
+    #  Test 1
+    #  Cannot add an entry with an undefined title.
     bad_data = {'text': 'test text'}
     journal.Entry.write(session=db_session, **bad_data)
     with pytest.raises(IntegrityError):
@@ -114,6 +127,8 @@ def test_entry_no_title_fails(db_session):
 
 
 def test_entry_no_text_fails(db_session):
+    #  Test 2
+    #  Cannot add an entry with undefined text.
     bad_data = {'title': 'test title'}
     journal.Entry.write(session=db_session, **bad_data)
     with pytest.raises(IntegrityError):
@@ -121,11 +136,15 @@ def test_entry_no_text_fails(db_session):
 
 
 def test_read_entries_empty(db_session):
+    #  Test 3
+    #  There will be zero entries at the beginning.
     entries = journal.Entry.all()
     assert len(entries) == 0
 
 
 def test_read_entries_one(db_session):
+    #  Test 4
+    #  Added entries are accessible.
     title_template = "Title {}"
     text_template = "Entry Text {}"
     # write three entries, with order clear in the title and text
@@ -142,7 +161,13 @@ def test_read_entries_one(db_session):
         assert isinstance(entry, journal.Entry)
 
 
+#  -----------------
+#  Test The Pages
+#  -----------------
+
 def test_empty_listing(app):
+    #  Test 5
+    #  Dispays helpful text when there are no entries.
     response = app.get('/')
     assert response.status_code == 200
     actual = response.body
@@ -151,6 +176,8 @@ def test_empty_listing(app):
 
 
 def test_listing(app, entry):
+    #  Test 6
+    #
     response = app.get('/')
     assert response.status_code == 200
     actual = response.body
@@ -160,6 +187,8 @@ def test_listing(app, entry):
 
 
 def test_post_to_add_view(app):
+    #  Test 7
+    #
     entry_data = {
         'title': 'Hello there',
         'text': 'This is a post',
@@ -172,29 +201,43 @@ def test_post_to_add_view(app):
 
 
 def test_add_no_params(app):
+    #  Test 8
+    #
     response = app.post('/add', status=500)
     assert 'IntegrityError' in response.body
 
 
+#  -----------------------
+#  Test the Authentication
+#  -----------------------
+
 def test_do_login_success(auth_req):
+    #  Test 9
+    #
     from journal import do_login
     auth_req.params = {'username': 'admin', 'password': 'secret'}
     assert do_login(auth_req)
 
 
 def test_do_login_bad_pass(auth_req):
+    #  Test 10
+    #
     from journal import do_login
     auth_req.params = {'username': 'admin', 'password': 'wrong'}
     assert not do_login(auth_req)
 
 
 def test_do_login_bad_user(auth_req):
+    #  Test 11
+    #
     from journal import do_login
     auth_req.params = {'username': 'bad', 'password': 'secret'}
     assert not do_login(auth_req)
 
 
 def test_do_login_missing_params(auth_req):
+    #  Test 12
+    #
     from journal import do_login
     for params in ({'username': 'admin'}, {'password': 'secret'}):
         auth_req.params = params
@@ -202,16 +245,9 @@ def test_do_login_missing_params(auth_req):
             do_login(auth_req)
 
 
-def login_helper(username, password, app):
-    """encapsulate app login for reuse in tests
-
-    Accept all status codes so that we can make assertions in tests
-    """
-    login_data = {'username': username, 'password': password}
-    return app.post('/login', params=login_data, status='*')
-
-
 def test_start_as_anonymous(app):
+    #  Test 14
+    #
     response = app.get('/', status=200)
     actual = response.body
     soup_actual = BeautifulSoup(actual)
@@ -221,6 +257,8 @@ def test_start_as_anonymous(app):
 
 
 def test_login_success(app):
+    #  Test 15
+    #
     username, password = ('admin', 'secret')
     redirect = login_helper(username, password, app)
     assert redirect.status_code == 302
@@ -234,6 +272,8 @@ def test_login_success(app):
 
 
 def test_login_fails(app):
+    #  Test 16
+    #
     username, password = ('admin', 'wrong')
     response = login_helper(username, password, app)
     assert response.status_code == 200
@@ -246,6 +286,8 @@ def test_login_fails(app):
 
 
 def test_logout(app):
+    #  Test 17
+    #
     # re-use existing code to ensure we are logged in when we begin
     test_login_success(app)
     redirect = app.get('/logout', status="3*")
